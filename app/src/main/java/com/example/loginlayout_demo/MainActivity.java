47cc9c26f9715fc2258f31854310c3597cc95e2d
Nhant <nhantk27023@gmail.com>
2023-09-26 17:26:42 +0700

    init project

---
+++ app/src/main/java/com/example/loginlayout_demo/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.loginlayout_demo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void signup(View view) {
+        startActivity(new Intent(this,signup.class));
+    }
+}
---
+++ app/src/main/java/com/example/loginlayout_demo/signup.java
@@ -0,0 +1,21 @@
+package com.example.loginlayout_demo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class signup extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+    }
+
+
+    public void signup(View view) {
+        startActivity(new Intent(this, signup.class));
+    }
+}
